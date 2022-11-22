87c3b7fd4b5767cf8768d6c13e5ceb3ffc07ce05
akramul2540 <akramul2540@gmail.com>
2022-11-22 21:40:53 +0600

    change

--- lib/main.dart
+++ lib/main.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_application_2/home_page.dart';
 import 'package:flutter_application_2/search_page.dart';
 
+
 void main (){
   runApp(MaterialApp(
     debugShowCheckedModeBanner: false,
